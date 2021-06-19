1a0ed1252f6af06e378ab004c2b57924e5ae32ae
Chaithanya Kanumolu <chaitu.kanumolu@gmail.com>
2021-06-19 19:05:07 +0530

    Adding basic gradle project

---
+++ src/main/java/com/missingperson/personfinder/PersonfinderApplication.java
@@ -0,0 +1,13 @@
+package com.missingperson.personfinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonfinderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonfinderApplication.class, args);
+	}
+
+}
